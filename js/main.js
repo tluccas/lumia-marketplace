e3db60b573c9798a1d2d7e10cf6c58a71025a971
Lucas Alves <lucas1901alves@gmail.com>
2025-11-21 20:20:10 -0300

    feat: main.js

---
+++ js/main.js
@@ -0,0 +1,30 @@
+import { fetchProdutos } from "./services/ApiService.js";
+import { setProducts } from "./components/state.js";
+import { renderProducts } from "./views/ProductView.js";
+import { setupFilters, applyFilters } from "./components/filters.js";
+import { setupModal } from "./views/ModalView.js";
+import { setupTheme } from "./controllers/ThemeController.js";
+import { criarProduto } from "./controllers/ProductController.js";
+
+async function init() {
+  setupTheme();
+  setupModal();
+  setupFilters();
+
+  // Carregar chatbot de forma opcional, se o arquivo existir
+  try {
+    const mod = await import("./controllers/ChatController.js");
+    if (typeof mod.default === "function") {
+      mod.default();
+    }
+  } catch (err) {
+    console.warn("[main] Chatbot opcional não carregado:", err?.message || err);
+  }
+
+  const data = await fetchProdutos();
+  setProducts(data);
+
+  criarProduto(data);
+  renderProducts(data);
+}
+init();
